c670882504405893071666f3949b3f4004141fc3
Danisaxas <danluismay@gmail.com>
2025-03-30 09:37:29 -0400

    update

--- templates/login.php
+++ templates/login.php
@@ -43,7 +43,7 @@
 </body>
 </html>
 <?php
-    require_once('../db/system_user.php');
+    require_once(__DIR__ . '/../db/system_user.php');
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $username = $_POST['username'];
         $contrasena = $_POST['contrasena'];
