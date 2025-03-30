fcbbd17b7494c32eb6add85412e194a3e5c0b37f
Danisaxas <danluismay@gmail.com>
2025-03-30 18:12:35 -0400

    a

--- index.php
+++ index.php
@@ -5,30 +5,42 @@ session_start();
 require_once 'db/system_user.php'; // Incluye el archivo de la base de datos
 
 // Define un array con las rutas permitidas
-$rutasPermitidas = ['register', 'login', 'home', 'logout', 'admin_codes']; // Agrega 'admin_codes'
+$rutasPermitidas = ['register', 'login', 'monedas', 'logout', 'admin_codes'];
 
 // Obtiene la ruta desde la URL
-$page = isset($_GET['page']) ? $_GET['page'] : 'home'; // Página por defecto a la página principal
+$ruta = isset($_GET['page']) ? $_GET['page'] : 'monedas'; // La página principal será monedas
+
+// Verifica si la ruta está permitida
+if (!in_array($ruta, $rutasPermitidas)) {
+    $ruta = 'login'; // Si no es válida, usa 'login' como fallback
+}
 
 // Output buffering
 ob_start();
 
 // Incluye el template correspondiente
-if ($page === 'register') {
+if ($ruta === 'register') {
     include 'templates/register.php';
-} elseif ($page === 'login') {
+} elseif ($ruta === 'login') {
     include 'templates/login.php';
-} elseif ($page === 'home') {
-    include 'templates/monedas.php';
-} elseif ($page === 'logout') {
+} elseif ($ruta === 'monedas') {
+    // Verificar si el usuario ha iniciado sesión antes de mostrar la página de monedas
+    if (isset($_SESSION['usuario_id'])) {
+        include 'templates/monedas.php';
+    } else {
+        // Redirige al usuario al login si no está logueado
+        header("Location: login");
+        exit();
+    }
+} elseif ($ruta === 'logout') {
     include 'templates/logout.php';
-} elseif ($page === 'admin_codes') { // Agrega el manejo de la nueva página
+} elseif ($ruta === 'admin_codes') { // Agrega el manejo de la nueva página
     // Verificar si el usuario es un OWNER
     if (isset($_SESSION['usuario_id']) && $_SESSION['username'] === 'AstroOwn') {
         include 'templates/admin_codes.php';
     } else {
         // Redirigir a una página de error o a la página principal
-        header("Location: index.php?page=home");
+        header("Location: monedas");
         exit();
     }
 } else {
